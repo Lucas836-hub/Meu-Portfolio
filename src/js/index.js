7e2076e1f1429013780b68355ad95376842954d6
lucas_git <70550900+Lucas836-hub@users.noreply.github.com>
2024-04-04 20:05:24 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+const btnMostrarProjetos = document.querySelector('.btn-mostrar-projetos');
+
+let quantidadeDeProjetos = 4;
+
+document.addEventListener("DOMContentLoaded", function () {
+  mostrarProjetos(quantidadeDeProjetos);
+});
+
+btnMostrarProjetos.addEventListener('click', mostrarMaisProjetos)
+
+function mostrarProjetos(quantidadeProjetos) {
+  const projetos = document.querySelectorAll('.projeto');
+
+  for (let i = 0; i < quantidadeProjetos; i++) {
+    if (projetos[i]) {
+      projetos[i].classList.add('ativo')
+    }
+  }
+}
+
+function mostrarMaisProjetos() {
+  const quantidadeTotalProjetos = document.querySelectorAll('.projeto').length;
+  quantidadeDeProjetos += 4; // Incrementa mais 4 projetos a serem mostrados
+  mostrarProjetos(quantidadeDeProjetos);
+
+  if (quantidadeDeProjetos >= quantidadeTotalProjetos) {
+    btnMostrarProjetos.classList.add('remover') // Esconde o botão se todos os projetos estiverem sendo mostrados
+  }
+}
